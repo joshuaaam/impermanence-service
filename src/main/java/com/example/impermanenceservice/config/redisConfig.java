d3834828f64975a3c77882b9fa8614c63eb58736
root <2253692565@qq.com>
2023-04-17 20:38:46 +0800

    feat: redisConfig

--- src/main/java/com/example/impermanenceservice/config/redisConfig.java
+++ src/main/java/com/example/impermanenceservice/config/redisConfig.java
@@ -1,8 +1,11 @@
 package com.example.impermanenceservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
@@ -10,9 +13,25 @@ import org.springframework.data.redis.serializer.GenericToStringSerializer;
 @Configuration
 public class redisConfig {
 
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private Integer port;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("110.41.151.59", 6389);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        config.setPassword(RedisPassword.of(password));
+
+        return new LettuceConnectionFactory(config);
+
+//        return new LettuceConnectionFactory("110.41.151.59", 6389 );
     }
 
     @Bean
